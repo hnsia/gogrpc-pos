b7a04cc6efd49a29381073f18f0b27af8b06a8ec
hnsia <44498178+hnsia@users.noreply.github.com>
2024-08-22 00:50:07 +0800

    feat: setup kitchen client

---
+++ services/kitchen/http.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/hnsia/gogrpc-pos/services/common/genproto/orders"
+)
+
+type httpServer struct {
+	addr string
+}
+
+func NewHttpServer(addr string) *httpServer {
+	return &httpServer{addr: addr}
+}
+
+func (s *httpServer) Run() error {
+	router := http.NewServeMux()
+
+	conn := NewGRPCClient(":9000")
+	defer conn.Close()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		c := orders.NewOrderServiceClient(conn)
+
+		ctx, cancel := context.WithTimeout(r.Context(), time.Second*2)
+		defer cancel()
+
+		_, err := c.CreateOrder(ctx, &orders.CreateOrderRequest{
+			CustomerID: 24,
+			ProductID:  3123,
+			Quantity:   2,
+		})
+		if err != nil {
+			log.Fatalf("client error: %v", err)
+		}
+	})
+
+	log.Println("Starting http server on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
+
+var ordersTemplate = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kitchen Orders</title>
+</head>
+<body>
+    <h1>Orders List</h1>
+    <table border="1">
+        <tr>
+            <th>Order ID</th>
+            <th>Customer ID</th>
+            <th>Quantity</th>
+        </tr>
+        {{range .}}
+        <tr>
+            <td>{{.OrderID}}</td>
+            <td>{{.CustomerID}}</td>
+            <td>{{.Quantity}}</td>
+        </tr>
+        {{end}}
+    </table>
+</body>
+</html>`
