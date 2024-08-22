0d984b97e11acfec17e2cfd5c88e733030a90fdc
hnsia <44498178+hnsia@users.noreply.github.com>
2024-08-23 01:36:53 +0800

    feat: Implement get orders

--- services/orders/handler/orders/grpc.go
+++ services/orders/handler/orders/grpc.go
@@ -41,3 +41,12 @@ func (h *OrdersGrpcHandler) CreateOrder(ctx context.Context, req *orders.CreateO
 
 	return res, nil
 }
+
+func (h *OrdersGrpcHandler) GetOrders(ctx context.Context, req *orders.GetOrdersRequest) (*orders.GetOrdersResponse, error) {
+	o := h.ordersService.GetOrders(ctx)
+	res := &orders.GetOrdersResponse{
+		Orders: o,
+	}
+
+	return res, nil
+}
